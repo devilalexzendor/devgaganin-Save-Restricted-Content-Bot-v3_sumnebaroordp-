769bfd881c7247eaa5387ea9a68abd89ba1f574a
sumnebaroordp <169070181+sumnebaroordp@users.noreply.github.com>
2025-06-02 22:51:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,21 +17,20 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
+API_ID = os.getenv("API_ID", "7041911")
+API_HASH = os.getenv("API_HASH", "abab2561c71e3004a55d4ff9763d5383")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_DB = os.getenv("MONGO_DB", "")
-OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split())) # list seperated via space
+MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://saved:saved@cluster0.r0js63z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "6390511215").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001234456")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-10012345567")) # optional with -100
-MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
-IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1001956199528")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002536566620")) # optional with -100
+MASTER_KEY = os.getenv("MASTER_KEY", "34924327c41ff8e7ad6334782f0198c6") # for session encryption
+IV_KEY = os.getenv("IV_KEY", "d9c4f17f682a") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
-
